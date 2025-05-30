7493486dc987e4ae6c4d4705e85cec7e0ea63762
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-31 00:34:49 +0700

    feat: Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-200 text-center">
+            <div className="text-red-500 p-4">
+                © 2022 Copyright:
+                <a className="text-gray-900" href="https://tailwind-elements.com/">Pitchakorn Thiprangsi</a>
+            </div>
+        </footer>
+    )
+}
